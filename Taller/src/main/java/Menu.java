609bf6e4362276d626bf78ca2011d93c3c0cd05b
Nacho <i.vallejos01@ufromail.cl>
2021-11-10 10:39:58 -0300

    10:00 -menú -Empresa -Sucursal

--- Taller/src/main/java/Menu.java
+++ Taller/src/main/java/Menu.java
@@ -9,7 +9,7 @@ public class Menu {
                 "5) desvincular camión\n" +
                 "6) calcular valor total");
     }
-    public int opción(){
+    public int opcion(){
         int num;
         do {
             Scanner tec = new Scanner(System.in);
@@ -17,5 +17,25 @@ public class Menu {
         }while(num<1||num>5);
             return num;
     }
-    public void
+    public void eleccion(){
+        Scanner tec = new Scanner(System.in);
+        Producto producto = new Producto();
+        switch (opcion()){
+            case 1:
+                producto.setProducto(tec);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                break;
+        }
+    }
 }
--- Taller/src/main/java/Sucursal.java
+++ Taller/src/main/java/Sucursal.java
@@ -7,4 +7,19 @@ public class Sucursal {
         this.region = region;
     }
 
+    public int getCodigoSucursal() {
+        return codigoSucursal;
+    }
+
+    public void setCodigoSucursal(int codigoSucursal) {
+        this.codigoSucursal = codigoSucursal;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
 }
